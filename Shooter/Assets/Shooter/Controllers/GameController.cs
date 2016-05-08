720fe22244b4f56538243cb73052ff9d8a135245
atil <hyperion91@gmail.com>
2016-05-08 22:33:10 +0300

    Initial commit;

---
+++ Shooter/Assets/Shooter/Controllers/GameController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+
+
+public class GameController : GameControllerBase {
+    
+    public override void InitializeGame(GameViewModel game) {
+    }
+}
---
+++ Shooter/Assets/Shooter/Controllers/ShipController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+
+
+public class ShipController : ShipControllerBase {
+    
+    public override void InitializeShip(ShipViewModel ship) {
+    }
+}
